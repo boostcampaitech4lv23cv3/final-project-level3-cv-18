1bb078f487e40dffc9c2e2d68bfd0f13bcc245b5
HyeongSeok Kim <tiryul@gmail.com>
2023-01-28 08:49:38 +0000

    Modify: main.py
     - config load 추가
     - RenderManager, CoordinateConverter 인스턴스 생성 테스트

--- ModelDeploy/main.py
+++ ModelDeploy/main.py
@@ -3,7 +3,9 @@ from argparse import Namespace
 from tqdm import tqdm
 import cv2
 import os
-
+import structures as st
+import json
+import numpy as np
 
 def parse_args() -> Namespace:
     parser = argparse.ArgumentParser(
@@ -13,9 +15,19 @@ def parse_args() -> Namespace:
     args = parser.parse_args()
     return args
 
+def load_json(path:str) -> dict:
+    with open(path, 'r') as file:
+        config = json.load(file)
+    return config
+
 def main(args:Namespace):
     cap = cv2.VideoCapture(os.path.join(args.path, args.file_format))
     images = [f for f in os.listdir(args.path) if f.endswith('.png')]
+    config = load_json('./config.json')
+    render_manager = st.RenderManager()
+    kitti_coordinate_converter = st.CoordinateConverter(cam2img=np.array(config['kitti']['matrix_camera_to_image']))
+
+    # inference loop
     pbar = tqdm(range(len(images)))
     progress = 0
     while(cap.isOpened()):
