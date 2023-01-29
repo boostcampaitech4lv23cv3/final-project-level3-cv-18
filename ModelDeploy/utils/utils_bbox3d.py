914913ff81c558f40d90bca9b0904c6126f82846
HyeongSeok Kim <tiryul@gmail.com>
2023-01-29 06:01:14 +0000

    Modify: utils_bbox3d.py
     - repecto code

--- ModelDeploy/utils/utils_bbox3d.py
+++ ModelDeploy/utils/utils_bbox3d.py
@@ -11,12 +11,11 @@ def create_bbox3d(result:st.InferenceResult) -> List[st.BoundingBox3D]:
     box_list = []
     for box, label, score in zip(bboxes, labels, scores):
         box_list.append(st.BoundingBox3D(pred_vector=box, label=label, score=score))
-    
     return box_list
     
     
 def project_bbox3d(converter:st.CoordinateConverter, bbox:st.BoundingBox3D) -> st.ProjectedBBox3D:
-    projected_bbox3d = converter.project_cam_to_image(bbox.corners.T)
+    projected_bbox3d = converter.project_cam_to_image(np.transpose(bbox.corners))
     pbbox = st.ProjectedBBox3D(projected_bbox3d=projected_bbox3d)
     return pbbox
 
