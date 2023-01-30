432b9e169ff999b3223f5eb4c19a0814b93beeb6
HyeongSeok Kim <tiryul@gmail.com>
2023-01-30 23:54:14 +0900

    Add: Asset.py

--- ModelDeploy/modules/__init__.py
+++ ModelDeploy/modules/__init__.py
@@ -5,3 +5,4 @@ from .BoundingBox3D import BoundingBox3D
 from .InferenceResult import InferenceResult
 from .ProjectedBBox3D import ProjectedBBox3D
 from .DataLoaderCV import DataLoaderCV
+from .Asset import Asset
