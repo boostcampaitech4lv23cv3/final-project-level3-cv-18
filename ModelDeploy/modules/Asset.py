edddd5014d138e76bcb73308efdeff9d0777710b
Jiyong-Jeon <jjy5136@naver.com>
2023-02-05 17:39:49 +0000

    Add: Calc trans_mat func

--- ModelDeploy/modules/Asset.py
+++ ModelDeploy/modules/Asset.py
@@ -1,11 +1,13 @@
 import json
 import os
 import numpy as np
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 from mmdet3d.structures.bbox_3d.cam_box3d import CameraInstance3DBoxes
+import cv2
 
 class Asset():
-    def __init__(self, path:str = "") -> None:
+    def __init__(self, path:str = "", down_ratio=4) -> None:
+        self.down_ratio = down_ratio
         if os.path.exists(path):
             self.load(path)
         else:
@@ -66,11 +68,53 @@ class Asset():
             self.input_path = path
             self.abs_path = path
             self.__apply_asset(asset_dict=asset_dict)
+    
+    
+    def get_ref_point(self, ref_point1: np.ndarray, ref_point2: np.ndarray) -> np.ndarray:
+        """Get reference point to calculate affine transform matrix.
+        While using opencv to calculate the affine matrix, we need at least
+        three corresponding points separately on original image and target
+        image. Here we use two points to get the the third reference point.
+        """
+        d = ref_point1 - ref_point2
+        ref_point3 = ref_point2 + np.array([-d[1], d[0]])
+        return ref_point3
+
+    # calc trans_mat
+    def get_transform_matrix(self, center:Tuple, scale:Tuple, output_scale:Tuple[float]) -> np.ndarray:
+        """
+        Get affine transform maxtirx.
+        Args:
+            center: Center of current image
+            scale: Scale of current image
+            output_scale: The transform target image scales
+        """
+        src_w = scale[0]
+        dst_w = output_scale[0]
+        dst_h = output_scale[1]
+        src_dir = np.array([0, src_w * -0.5])
+        dst_dir = np.array([0, dst_w * -0.5])
+        src = np.zeros((3, 2), dtype=np.float32)
+        dst = np.zeros((3, 2), dtype=np.float32)
+        src[0, :] = center
+        src[1, :] = center + src_dir
+        dst[0, :] = np.array([dst_w * 0.5, dst_h * 0.5])
+        dst[1, :] = np.array([dst_w * 0.5, dst_h * 0.5]) + dst_dir
+        src[2, :] = self.get_ref_point(src[0, :], src[1, :])
+        dst[2, :] = self.get_ref_point(dst[0, :], dst[1, :])
+        get_matrix = cv2.getAffineTransform(src, dst)
+        matrix = np.concatenate((get_matrix, [[0., 0., 1.]]))
+        return matrix.astype(np.float32)
 
     def load(self, path:str):
         with open(path, 'r') as file:
             asset_dict = json.load(file)
-
+        if not "trans_mat" in asset_dict.keys():
+            asset_dict["trans_mat"] = self.get_transform_matrix(
+                    (asset_dict["original_size"][0]/2, asset_dict["original_size"][1]/2), 
+                    (asset_dict["original_size"][0], asset_dict["original_size"][1]),
+                    (asset_dict["input_size"][0]//self.down_ratio, asset_dict["input_size"][1]//self.down_ratio))
+        print(asset_dict)
         self.input_path = path
         self.abs_path = os.path.abspath(path)
         self.__apply_asset(asset_dict=asset_dict)
